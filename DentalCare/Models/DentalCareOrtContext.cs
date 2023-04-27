a0d9dde203771b2799f2f9137023015bede2722c
Christian Viarnes <christiancdv@gmail.com>
2023-04-26 23:40:46 -0300

    se crea migracion a base de datos

---
+++ DentalCare/Models/DentalCareOrtContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DentalCare.Models;
+
+public partial class DentalCareOrtContext : DbContext
+{
+    public DentalCareOrtContext()
+    {
+    }
+
+    public DentalCareOrtContext(DbContextOptions<DentalCareOrtContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Reserva> Reservas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=localhost; database=DentalCareORT; integrated security=true; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Reserva>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Reservas__3214EC07A00D7735");
+
+            entity.Property(e => e.Especialidad)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Especialista)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.Fecha).HasColumnType("datetime");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DentalCare/Models/Reserva.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DentalCare.Models;
+
+public partial class Reserva
+{
+    public int Id { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public string Especialidad { get; set; } = null!;
+
+    public string Especialista { get; set; } = null!;
+}
